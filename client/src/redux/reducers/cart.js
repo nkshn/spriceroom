08776c6b818f6f2932d09e19ec35a579a8ff60c1
nkshn <vovanika23413@gmail.com>
2021-05-16 18:16:19 +0300

    add localStorage to cart

--- client/src/redux/reducers/cart.js
+++ client/src/redux/reducers/cart.js
@@ -1,8 +1,10 @@
 import * as actionTypes from "./../types/cart";
 
+const CART_DATA_KEY = "cartData"; // name of localStorage key
+
 const initialState = {
-  products: [],
-  totalCost: 0,
+  products: JSON.parse(localStorage.getItem(CART_DATA_KEY)).products || [],
+  totalCost: JSON.parse(localStorage.getItem(CART_DATA_KEY)).cost || 0,
   error: "",
   isSending: false
 }
@@ -10,30 +12,35 @@ const initialState = {
 const cartReducer = function (state = initialState, action) {
   switch (action.type) {
     case actionTypes.ADD_TO_CART:
+      localStorage.setItem(CART_DATA_KEY, JSON.stringify({ products: action.payload.products, cost: action.payload.totalCart }));
       return {
         ...state,
         products: action.payload.products,
         totalCost: action.payload.totalCart,
       }
     case actionTypes.DECREASE_ITEM_QTY:
+      localStorage.setItem(CART_DATA_KEY, JSON.stringify({ products: action.payload.products, cost: action.payload.totalCart }));
       return {
         ...state,
         products: action.payload.products,
         totalCost: action.payload.totalCart,
       }
     case actionTypes.INCREASE_ITEM_QTY:
+      localStorage.setItem(CART_DATA_KEY, JSON.stringify({ products: action.payload.products, cost: action.payload.totalCart }));
       return {
         ...state,
         products: action.payload.products,
         totalCost: action.payload.totalCart,
       }
     case actionTypes.REMOVE_FROM_CART:
+      localStorage.setItem(CART_DATA_KEY, JSON.stringify({ products: action.payload.products, cost: action.payload.totalCart }));
       return {
         ...state,
         products: action.payload.products,
         totalCost: action.payload.totalCart,
       }
     case actionTypes.CLEAR_CART:
+      localStorage.setItem(CART_DATA_KEY, JSON.stringify({ products: [], cost: 0 }));
       return {
         ...state,
         products: [],
@@ -42,7 +49,8 @@ const cartReducer = function (state = initialState, action) {
     case actionTypes.SUBMIT_CART:
       return { ...state, isSending: true, error: "" }
     case actionTypes.SUBMIT_CART_SUCCESS:
-      return { ...state, isSending: false, products: [], totalCost: 0 }
+      localStorage.setItem(CART_DATA_KEY, JSON.stringify({ products: [], cost: 0 }));
+      return { ...state, isSending: false, products: [], cost: 0 }
     case actionTypes.SUBMIT_CART_FAILURE:
       return { ...state, isSending: false, error: action.payload.msg }
     default:
